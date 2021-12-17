6f2dcd8290ffe5b23b2629b89810c2f98b291475
Conservatism <2628559678@qq.com>
2021-12-17 10:49:02 +0800

    variance introduce

--- tennissimulationmodel.py
+++ tennissimulationmodel.py
@@ -2,8 +2,12 @@ import random
 import re
 import urllib.request
 from bs4 import BeautifulSoup
+import json
 import xlrd
 import time
+import numpy as np
+import copy
+import pandas as pd
 
 def getplayerinfo(playername):
     file = "UTS_playerinfo.xls"
@@ -41,9 +45,65 @@ def get_elo(playerid,surface):
         elo[0]="1600"
     return elo[0]
 
+def getvariance(playerid,surface,opponent):
+    url = "https://www.ultimatetennisstatistics.com/matchesTable?playerId="+playerid+"&current=1&rowCount=15&sort%5Bdate%5D=desc&searchPhrase=&season=&fromDate=&toDate=&level=&bestOf=&surface="+surface+"&indoor=&speed=&round=&result=&opponent=&tournamentId=&tournamentEventId=&outcome=&score=&countryId=&bigWin=false&_="
+    head = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3756.400 QQBrowser/10.5.4039.400",
+        #"sec-ch-ua": '"Chromium";v ="92","Not A;Brand";v="99","Google Chrome";v="92"',
+        #"Cookie": "_ga = GA1.2.324355889.1621857694;__gads=ID=8cfa7d12bbca2f6d-22ba21761bc80082:T=1621857691:RT = 1621857691:S=ALNI_MYJRBxeLS0iZna4gzE_F2cbdf8ZDA;gid = GA1.2.1014988429.1630369661"
+    }
+    req = urllib.request.Request(url, headers=head)
+    print("1")
+    response = urllib.request.urlopen(req)
+    html = response.read().decode('utf-8')
+    print("1")
+    conlist = json.loads(html)
+    matchlist = []
+    wollist = []
+    stats = np.zeros((8, 10), dtype=float)
+    for i in range(0, 10):
+        matchlist.append(conlist["rows"][i]["id"])
+        wollist.append((conlist["rows"][i]["winner"]["id"] == int(playerid)))
+        #print(conlist["rows"][i]["id"])
+        # print(wollist[i])
+    s = 0
+    i = 0
+    sigma = []
+    while s < 8:
+        j = 0
+        time.sleep(0.2)
+        url = "https://www.ultimatetennisstatistics.com/matchStats?matchId=" + str(matchlist[i])
+        req = urllib.request.Request(url, headers=head)
+        response = urllib.request.urlopen(req)
+        html = response.read().decode('utf-8')
+        bs = BeautifulSoup(html, "html.parser")
+        if wollist[i] == True:
+            list = bs.select("th[class='text-" + "right" + " pct-data']", limit=10)
+        else:
+            list = bs.select("th[class='text-" + "left" + " pct-data']", limit=10)
+        print(list)
+        if list == []:
+            i = i + 1
+            continue
+        for m in list:
+            # print(m.text)
+            if m.text:
+                value = float(m.text.strip("%"))
+                stats[s][j] = value
+                j = j + 1
+            else:
+                value = np.NaN
+                stats[s][j] = value
+                j = j + 1
+        i = i + 1
+        s = s + 1
+    for i in range(0, 10):
+        sigma.append(pd.DataFrame(stats[:, i]).std(ddof=0))
+    return sigma
+
 def getstats(playerid,surface,opponent,type):
     baseurl1="https://www.ultimatetennisstatistics.com/matchesStats?playerId="
-    baseurl2="&season=&fromDate=08-02-2021&toDate=&level="
+    baseurl2="&season=&fromDate=01-07-2021&toDate=&level="
     baseurl3="&bestOf=&surface="
     baseurl4="&indoor=&speed=&round=&result=&opponent="
     baseurl5="&tournamentId=&tournamentEventId=&outcome=&score=&countryId=&bigWin=false&searchPhrase="
@@ -140,11 +200,13 @@ def play(player1,player2,g1,g2,p1,p2,num):
             p1=p1+1
             #print(g1," ",g2,player1.name,"save!")
     else:
-        if p<=player1.ace/100+random.uniform(stochastic(num)-0.025,stochastic(num)+0.01):
+        if p<=player1.ace/100+random.uniform(stochastic(num)-0.01,stochastic(num)+0.01):
+        #if p <= player1.ace / 100:
             p1=p1+1
             #print(g1, " ", g2, " ", player1.name, p1, "ace!")
         else:
-            if p>player1.firstin/100+random.uniform(stochastic(num)-0.06,stochastic(num)+0.06):
+            if p>player1.firstin/100+random.uniform(stochastic(num)-0.03,stochastic(num)+0.03):
+            #if p > player1.firstin / 100:
                 if p>1-player2.doublefault/100:
                     p2=p2+1
                     #print(g1, " ", g2," " , player1.name,p1,"df!")
@@ -152,6 +214,7 @@ def play(player1,player2,g1,g2,p1,p2,num):
                     pr2=random.random()
                     #prob=player1.secondserve*(100-player2.secondreturn)/(player1.secondserve*(100-player2.secondreturn)+(100-player1.secondserve)*player2.secondreturn)
                     prob=player1.secondserve/100*weigh+(1-player2.secondreturn/100)*(1-weigh)+random.uniform(stochastic(num)-0.05,stochastic(num)+0.02)
+                    #prob = player1.secondserve / 100 * weigh + (1 - player2.secondreturn / 100) * (1 - weigh)
                     #print(player1.name,prob)
                     if pr2>prob:
                         p2=p2+1
@@ -160,15 +223,16 @@ def play(player1,player2,g1,g2,p1,p2,num):
             else:
                 pr1=random.random()
                 #prob=player1.firstserve*(100-player1.firstreturn)/(player1.firstserve*(100-player2.firstreturn)+(100-player1.firstserve)*player2.firstreturn)
-                prob=player1.firstserve/100*weigh+(1-player2.firstreturn/100)*(1-weigh)+random.uniform(stochastic(num)-0.05,stochastic(num)+0.04)
+                prob=player1.firstserve/100*weigh+(1-player2.firstreturn/100)*(1-weigh)+random.uniform(stochastic(num)-0.04,stochastic(num)+0.04)
+                #prob = player1.firstserve / 100 * weigh + (1 - player2.firstreturn / 100) * (1 - weigh)
                 if pr1>prob:
                     p2=p2+1
                 else:
                     p1=p1+1
-    return [p1,p2]
+    return [p1,p2,g1,g2]
 
 def playseven(player1,player2,p1,p2,i):
-    weigh = (player1.elo - 1500) / (player1.elo + player2.elo - 3000)
+    weigh = (player1.elo - 1600) / (player1.elo + player2.elo - 3200)
     p=random.random()
     if p <= player1.ace / 100:
         p1 = p1 + 1
@@ -193,17 +257,39 @@ def playseven(player1,player2,p1,p2,i):
     i=i+1
     return [p1,p2,i]
 
-def playset(player1,player2):
+def change(player,var):
+    #print_obj(player)
+
+    player.ace=player.ace+np.random.normal(loc =0.0 , scale= var[0])
+    player.doublefault = player.doublefault + np.random.normal(loc=0.0, scale=var[1])
+    player.firstin=player.firstin+np.random.normal(loc=0.0, scale=var[2])
+    player.firstserve=player.firstserve+np.random.normal(loc=0.0, scale=var[3])
+    player.secondserve=player.secondserve+np.random.normal(loc=0.0, scale=var[4])
+    player.breaksave=player.breaksave+np.random.normal(loc=0.0, scale=var[5])
+    player.firstreturn=player.firstreturn+np.random.normal(loc=0.0, scale=var[7])
+    player.secondreturn=player.secondreturn+np.random.normal(loc=0.0, scale=var[8])
+    player.breakconvert=player.breakconvert+np.random.normal(loc=0.0, scale=var[9])
+
+    return player
+
+def playset(player1,player2,var1,var2,setmode):
     setpoint1 = 0
     setpoint2 = 0
     order = 1
-    #while setpoint1 != 3 and setpoint2 != 3:
-    while setpoint1 != 2 and setpoint2 != 2:
+    player11=copy.deepcopy(player1)
+    player22=copy.deepcopy(player2)
+    player11=change(player11,var1)
+    #print(player1==player11)
+    player22=change(player22, var2)
+    #print_obj(player22)
+    lengame = 0
+    while setpoint1 != setmode and setpoint2 != setmode:
         gamepoint1 = 0
         gamepoint2 = 0
+
         while max(gamepoint1, gamepoint2) < 6 or (
                 max(gamepoint1, gamepoint2) == 6 and min(gamepoint1, gamepoint2) >= 5):
-            list = game(player1, player2, gamepoint1, gamepoint2, order, setpoint1 + setpoint2 + 1)
+            list = game(player11, player22, gamepoint1, gamepoint2, order, setpoint1 + setpoint2 + 1)
             gamepoint1 = list[0]
             gamepoint2 = list[1]
             order = -1 * order
@@ -212,94 +298,120 @@ def playset(player1,player2):
             setpoint1 = setpoint1 + 1
         else:
             setpoint2 = setpoint2 + 1
+        lengame=lengame+gamepoint1-gamepoint2
     #     if (gamepoint1 == 6 and gamepoint2 == 7) or (gamepoint2 == 6 and gamepoint1 == 7):
     #         print(gamepoint1, "vs", gamepoint2,"(",min(list[2],list[3]),")")
     #     else:
     #         print(gamepoint1, "vs", gamepoint2)
     #
     # print(setpoint1,":",setpoint2)
-    return [setpoint1,setpoint2]
+    return [setpoint1,setpoint2,lengame]
 
 def print_obj(obj):
     print (['%s:%s' % item for item in obj.__dict__.items()])
 
-if __name__ == '__main__':
-
-    type1=""
-    type2 = ""
-    playername1="Opelka"
-    playername2="Muirhead Harris"
-    surface = "H"
-    opponent1 = "TOP_50"
-    opponent2 = "TOP_50"
-
+def match(playername1,playername2,surface,opponent1,opponent2,type1,type2,setmode,N):
     playerid1 = str(int(getplayerinfo(playername1)))
     playerid2 = str(int(getplayerinfo(playername2)))
-    if playerid2=="-1" or playerid1=="-1":
+    if playerid2 == "-1" or playerid1 == "-1":
         exit(1)
-    stats1=getstats(playerid1,surface,opponent1,type1)
+    stats1 = getstats(playerid1, surface, opponent1, type1)
     print("Information 1 has been collected")
-    time.sleep(2)
-    player1 = player(playername1, stats1[0], stats1[2], stats1[3], stats1[1], stats1[4], stats1[5], stats1[10], stats1[11],
-                     stats1[12], int(get_elo(playerid1,surface)))
+    time.sleep(4)
+
+    var1=[7.38,2.97,6.16,8.97,19.03,21.923,10.865,8.9606,10.6145,21.0954]
+    #var2 = [4.754, 4.1877, 7.1376, 9.6336, 13.2930, 33.8474, 10.4223, 8.8171, 8.5922, 30.9808]
+
+    player1 = player(playername1, stats1[0], stats1[2], stats1[3], stats1[1], stats1[4], stats1[5], stats1[10],
+                     stats1[11],
+                     stats1[12], int(get_elo(playerid1, surface)))
     print_obj(player1)
-    time.sleep(2)
-    stats2=getstats(playerid2,surface,opponent2,type2)
+    time.sleep(0.5)
+    stats2 = getstats(playerid2, surface, opponent2, type2)
     print("Information 2 has been collected")
-    time.sleep(2)
-    player2 = player(playername2, stats2[0], stats2[2], stats2[3], stats2[1], stats2[4], stats2[5], stats2[10], stats2[11],
-                     stats2[12], int(get_elo(playerid2,surface)))
+    time.sleep(8)
+    #var1 = getvariance(playerid1, surface, opponent1)
+    print(var1)
+    var2 = getvariance(playerid2, surface, opponent2)
+    print(var2)
+    player2 = player(playername2, stats2[0], stats2[2], stats2[3], stats2[1], stats2[4], stats2[5], stats2[10],
+                     stats2[11],
+                     stats2[12], int(get_elo(playerid2, surface)))
     print_obj(player2)
+
     list1 = []
     list2 = []
-    sum30 = 0
-    sum31 = 0
-    sum32 = 0
-    sum23 = 0
-    sum13 = 0
-    sum03 = 0
-    sum20 = 0
-    sum21 = 0
-    sum12 = 0
-    sum02 = 0
-    N=10000
-
+    list3 = []
     print("start to simulate...")
     for i in range(0, N):
-        list = playset(player1, player2)
+        list = playset(player1, player2, var1, var2, setmode)
         list1.append(list[0])
         list2.append(list[1])
-    for i in range(0, N):
-        # if list1[i] == 3 and list2[i] == 0:
-        #     sum30 += 1
-        # elif list1[i] == 3 and list2[i] == 1:
-        #     sum31 += 1
-        # elif list1[i] == 3 and list2[i] == 2:
-        #     sum32 += 1
-        # elif list1[i] == 2 and list2[i] == 3:
-        #     sum23 += 1
-        # elif list1[i] == 1 and list2[i] == 3:
-        #     sum13 += 1
-        # else:
-        #     sum03 += 1
+        list3.append(list[2])
 
-        if list1[i] == 2 and list2[i] == 0:
-            sum20 += 1
-        elif list1[i] == 2 and list2[i] == 1:
-            sum21 += 1
-        elif list1[i] == 1 and list2[i] == 2:
-            sum12 += 1
-        else:
-            sum02 += 1
+    if setmode==2:
+        sum20 = 0
+        sum21 = 0
+        sum12 = 0
+        sum02 = 0
+        for i in range(0, N):
+            if list1[i] == 2 and list2[i] == 0:
+                sum20 += 1
+            elif list1[i] == 2 and list2[i] == 1:
+                sum21 += 1
+            elif list1[i] == 1 and list2[i] == 2:
+                sum12 += 1
+            else:
+                sum02 += 1
 
-    # print(player1.name,"3:0",player2.name, sum30 / N)
-    # print(player1.name,"3:1",player2.name, sum31 / N)
-    # print(player1.name,"3:2",player2.name, sum32 / N)
-    # print(player1.name,"2:3",player2.name, sum23 / N)
-    # print(player1.name,"1:3",player2.name, sum13 / N)
-    # print(player1.name,"0:3",player2.name, sum03 / N)
+        print(player1.name, "2:0", player2.name, sum20 / N)
+        print(player1.name, "2:1", player2.name, sum21 / N)
+        print(player1.name, "1:2", player2.name, sum12 / N)
+        print(player1.name, "0:2", player2.name, sum02 / N)
 
-    print(player1.name, "2:0", player2.name, sum20 / N)
-    print(player1.name, "2:1", player2.name, sum21 / N)
-    print(player1.name, "1:2", player2.name, sum12 / N)
-    print(player1.name, "0:2", player2.name, sum02 / N)
+    else:
+        sum30 = 0
+        sum31 = 0
+        sum32 = 0
+        sum23 = 0
+        sum13 = 0
+        sum03 = 0
+
+        for i in range(0, N):
+            if list1[i] == 3 and list2[i] == 0:
+                sum30 += 1
+            elif list1[i] == 3 and list2[i] == 1:
+                sum31 += 1
+            elif list1[i] == 3 and list2[i] == 2:
+                sum32 += 1
+            elif list1[i] == 2 and list2[i] == 3:
+                sum23 += 1
+            elif list1[i] == 1 and list2[i] == 3:
+                sum13 += 1
+            else:
+                sum03 += 1
+
+        print(player1.name,"3:0",player2.name, sum30 / N)
+        print(player1.name,"3:1",player2.name, sum31 / N)
+        print(player1.name,"3:2",player2.name, sum32 / N)
+        print(player1.name,"2:3",player2.name, sum23 / N)
+        print(player1.name,"1:3",player2.name, sum13 / N)
+        print(player1.name,"0:3",player2.name, sum03 / N)
+
+    lenlist=pd.DataFrame(list3)
+    lenlist[1]=1
+    len=lenlist.groupby(0).count()/N
+    print(len)
+
+if __name__ == '__main__':
+
+    type1="" #match type
+    type2 = ""
+    playername1="Djokovic"
+    playername2="Medvedev"
+    surface = "H"   #court type
+    opponent1 = ""
+    opponent2 = ""
+    setmode=3
+    N = 10000
+    match(playername1, playername2, surface, opponent1, opponent2, type1, type2, setmode, N)
